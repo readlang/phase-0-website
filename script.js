ce1b9189265e3f549dd883bda51b9fac1ad7f642
Read Langworthy <rlangworthy@gmail.com>
2021-09-30 13:01:48 -0700

    before changing to css grid

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const h2 = document.createElement("h2");
 h2.textContent = "This content added by Javascript";
 
-document.querySelector("body").appendChild(h2);
+//document.querySelector("body").appendChild(h2);
 
